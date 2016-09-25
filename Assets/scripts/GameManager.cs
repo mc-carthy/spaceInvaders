95fbbf0c3b0e5b48acba79f6b554dcdace890997
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-25 11:35:36 +0100

    Add enemyKill and gameOver conditions to GameManger

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -15,13 +15,45 @@ public class GameManager : MonoBehaviour {
 	private int score;
 	private int enemyCount;
 	private float gameTimer;
-	private bool gameOver;
+	private bool gameover;
 
 	private void Start () {
 		Time.timeScale = 1;
 
-		player.onHitEnemy += onGameOver;
-		player.onKillEnemy += onKillEnemy;
+		player.onHitEnemy += OnGameOver;
+		player.onKillEnemy += OnKillEnemy;
 		enemyCount = enemyGroup.GetComponentsInChildren<Enemy> ().Length;
+	}
+
+	private void Update () {
+		if (gameover) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().name, LoadSceneMode.Single);
+			}
+			return;
+		}
+		scoreText.text = "Score: " + score;
+	}
+
+	private void OnKillEnemy () {
+		score += 100;
+		enemyCount--;
+		if (enemyCount <= 0) {
+			OnGameOver ();
+		}
+	}
+
+	private void OnGameOver () {
+		gameover = true;
+		scoreText.enabled = false;
+		gameoverText.enabled = true;
+
+		if (enemyCount != 0) {
+			gameoverText.text = "Game Over!\nScore: " + score + "\nPress R to Restart";
+		} else {
+			gameoverText.text = "You Win!\nScore: " + score + "\nPress R to Restart";
+		}
+
+		Time.timeScale = 0;
 	}
 }
