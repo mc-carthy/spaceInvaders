6f748fdea6482033980d00c4599e9005de09061b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-25 11:13:29 +0100

    Create Bullet class

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	private float speed = 10;
+	private Action _onKillEnemy;
+	public Action onKillEnemy { set { this._onKillEnemy = value; } }
+
+	private void Update () {
+		transform.position = new Vector3 (
+			transform.position.x, 
+			transform.position.y + speed * Time.deltaTime,
+			transform.position.z
+		);
+
+		if (transform.position.y > 6) {
+			Destroy (gameObject);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void OnTriggerEnter2D (Collider2D trig) {
+		if (trig.gameObject.GetComponent<Enemy> () != null) {
+			_onKillEnemy ();
+			Destroy (trig.gameObject);
+			Destroy (gameObject);
+		}
 	}
 }
--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,13 +3,5 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
