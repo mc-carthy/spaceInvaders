7b0acc3fd0a650cfd19f367829320af49c474bc6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-25 11:28:01 +0100

    Add GameManager

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,7 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	
+	private Camera mainCam;
+	[SerializeField]
+	private Text scoreText, gameoverText;
+	[SerializeField]
+	private Player player;
+	[SerializeField]
+	private EnemyGroup enemyGroup;
+	private int score;
+	private int enemyCount;
+	private float gameTimer;
+	private bool gameOver;
+
+	private void Start () {
+		Time.timeScale = 1;
+
+		player.onHitEnemy += onGameOver;
+		player.onKillEnemy += onKillEnemy;
+		enemyCount = enemyGroup.GetComponentsInChildren<Enemy> ().Length;
+	}
 }
